580cab31ad3c16337d30aa4a62679e6bb0865d16
Mr-vero <Mr.vero@dr.com>
2016-09-24 19:22:38 +0700

    Add files via upload

---
+++ application/controllers/admin/BaseController.class.php
@@ -0,0 +1,15 @@
+<?php 
+/*
+*  Author :  Irvan
+*  Version : 0.0.1
+*  Last Modified: September 17, 2016
+*  Path : application/controllers/admin/BaseController.class.php
+*/ 
+class FController 
+{
+	
+	function __construct()
+	{
+		require CONFIG_PATH. "config.php";
+	}
+}
---
+++ application/controllers/admin/IndexController.class.php
@@ -0,0 +1,14 @@
+<?php
+/*
+*  Author :  Irvan
+*  Version : 0.0.1
+*  Last Modified: September 17, 2016
+*  Path : application/controllers/admin/IndexController.class.php
+*/ 
+class IndexController extends FController{
+
+    public function indexAction(){
+      //
+    }
+
+}
