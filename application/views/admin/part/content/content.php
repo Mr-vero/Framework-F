65e13ac4df5258731a0c9ef349bac3948080b913
Mr-vero <Mr.vero@dr.com>
2016-09-24 19:51:06 +0700

    Update content.php

--- application/views/admin/part/content/content.php
+++ application/views/admin/part/content/content.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="content">
             <div class="title">Framework F</div>
-            <div class="quote"><?php echo $title = $lang['admin-title'] ?></div><hr>
+            <div class="quote"><?php echo $title = $lang['admin-title'] ?></div>
         </div>
     </div>
 </body>
