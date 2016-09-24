1b0e1185cd5114100a5e67fb4213f428268dfb7e
Irvan <Mr.vero@dr.com>
2016-09-25 01:13:46 +0700

    Update head.php

--- application/views/home/part/head/head.php
+++ application/views/home/part/head/head.php
@@ -1,6 +1,6 @@
 <html>
     <head>
-        <link href='//fonts.googleapis.com/css?family=Henny+Penny' rel='stylesheet' type='text/css'>
+        <link href='//fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
 
         <style>
             body {
@@ -8,11 +8,12 @@
                 padding: 0;
                 width: 100%;
                 height: 100%;
-                color: #B0BEC5;
+                color: #2b2b2b;
                 display: table;
                 font-weight: 50;
-                font-family: 'Henny Penny', serif;
-                background: rgb(42,42,42);
+                font-family: 'Pacifico', serif;
+                background: url('public/images/bg.png');
+                text-shadow: 3px 3px 0px rgba(0,0,0,0.1), 7px 7px 0px rgba(0,0,0,0.05);
             }
 
             .container {
