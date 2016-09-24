3525afa41e25dbd1e4a1f9d035179eaac4024511
Mr-vero <Mr.vero@dr.com>
2016-09-24 19:49:28 +0700

    Update content.php

--- application/views/admin/part/content/content.php
+++ application/views/admin/part/content/content.php
@@ -3,9 +3,6 @@
         <div class="content">
             <div class="title">Framework F</div>
             <div class="quote"><?php echo $title = $lang['admin-title'] ?></div><hr>
-            <a href=<?php echo BASE_URL."home/index/index" ?>>Test Home Controller</a><br>
-            <a href=<?php echo BASE_URL."admin/index/index" ?>>Test Admin Controller</a> 
-            <a href=<?php echo BASE_URL."home/index/index/indonesia"; ?>>Bahasa Indonesia</a>
         </div>
     </div>
 </body>
