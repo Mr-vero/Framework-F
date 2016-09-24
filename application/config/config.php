dbe147eca59c15cbc5eddcc702d8471691019faa
Mr-vero <Mr.vero@dr.com>
2016-09-24 19:20:22 +0700

    Add files via upload

---
+++ application/config/config.php
@@ -0,0 +1,29 @@
+<?php 
+/*
+*  Author :  Irvan
+*  Version : 0.0.1
+*  Last Modified: September 17, 2016
+*  Path : application/config/config.php
+--------------------------------------------*/
+	return(
+		$config = array(
+		'host' => "localhost",
+
+		'user' => "root",
+		
+		'password' => "",
+		
+		'dbname' => "",
+		
+		'port' => "3306",
+		
+		'charset' => '',
+		
+		'prefix' => '',
+		)
+	);
+/*--------------------------------------------
+* Start File
+* Default is views/home/index.php
+*/
+?>
---
+++ application/config/default.php
@@ -0,0 +1,19 @@
+<?php 
+/*
+*  Author :  Irvan
+*  Version : 0.0.1
+*  Last Modified: September 17, 2016
+*  Path : config/default.php
+--------------------------------------------------------------*/
+	$url = "http://localhost/f/"; //Change this link
+	//$lang = "english"; //Default Language (static);
+	$lang = $lang ? $lang = $lang : $lang = "english"; //Dynamic Language; 
+/*-------------------------------------------------------------
+|PLEASE DO NOT CHANGE THIS PART
+|THIS IS THE DEFAULT CONFIGURATION
+---------------------------------------------------------------------
+---------------------------------------------------------------------*/
+	define("BASE_URL", $url); 
+	isset($lang) ? $GLOBALS['lang'] =  include LANGUAGE . $lang . ".php" : $GLOBALS['lang'] = include LANGUAGE . "english.php"; 
+/*--------------------------------------------------------------------*/
+ ?>
