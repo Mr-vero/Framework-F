2eef3bfaf64b9d1fa0c3411b7a10e433f1451d46
Mr-vero <Mr.vero@dr.com>
2016-09-24 19:38:57 +0700

    Add files via upload

---
+++ application/views/home/part/content/content.php
@@ -0,0 +1,9 @@
+    <div class="container">
+        <div class="content">
+            <div class="title">Framework F</div>
+            <div class="quote"><?php echo $title = $lang['title'] ?></div><hr>
+            <a href=<?php echo BASE_URL."home/index/index" ?>>Test Home Controller</a><br>
+            <a href=<?php echo BASE_URL."admin/index/index" ?>>Test Admin Controller</a> 
+            <a href=<?php echo BASE_URL."home/index/index/indonesia"; ?>>Bahasa Indonesia</a>
+        </div>
+    </div>
