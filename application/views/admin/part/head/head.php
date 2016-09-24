a05a0447217c6c71c523cb78409eca564ea63060
Mr-vero <Mr.vero@dr.com>
2016-09-24 19:35:08 +0700

    Add files via upload

---
+++ application/views/admin/part/head/head.php
@@ -0,0 +1,46 @@
+<html>
+    <head>
+        <link href='//fonts.googleapis.com/css?family=Cinzel' rel='stylesheet' type='text/css'>
+
+        <style>
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                height: 100%;
+                color: #B0BEC5;
+                display: table;
+                font-weight: 50;
+                font-family: 'Cinzel', serif;
+                background: rgb(42,42,42);
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 96px;
+                margin-bottom: 40px;
+            }
+
+            .quote {
+                font-size: 24px;
+            }
+
+            a{
+                color: yellow;
+                text-decoration: none;
+            }
+            a:hover{
+                color:white;
+            }
+        </style>
+    </head>
