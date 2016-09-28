f32defadbbbfc7a2c26937db0bb164ae0d58c90f
Irvan <Mr.vero@dr.com>
2016-09-28 13:00:13 +0700

    Update fruit_helper.php

--- engine/helpers/fruit_helper.php
+++ engine/helpers/fruit_helper.php
@@ -1,6 +1,6 @@
 <?php
 @require ('/vendor/autoload.php');
-require ('/vendor/fruit/fruit/fruit.php');
+require ('/vendor/fruit/f/fruit.php');
 if (!function_exists('fruit_get_instance'))
 {
     function fruit_get_instance($name = false)
