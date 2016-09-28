192d485cf1798282a3db481d358ad8132c413cd5
Irvan <Mr.vero@dr.com>
2016-09-28 13:13:32 +0700

    Delete xcrud_helper.php

--- engine/helpers/xcrud_helper.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once ('/xcrud/xcrud.php');
-if (!function_exists('xcrud_get_instance'))
-{
-    function xcrud_get_instance($name = false)
-    {
-        return Xcrud::get_instance($name);
-    }
-}
